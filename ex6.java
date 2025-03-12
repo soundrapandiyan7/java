6fb2dbefddef878ee27c5fd93b59233aeb3587ac
soundrapanndiyan7 <soundrapanndiyan2468@gmail.com>
2025-03-12 15:27:09 +0530

    hari

---
+++ ex6.java
@@ -0,0 +1,7 @@
+public class ex6{
+    public static void main(String[]agrs){
+       String firstname="Hari";
+       String secondname="Haran";
+        System.out.print(firstname.concat(secondname));
+    }
+}
