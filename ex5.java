01cce40ae1dc093b68987afdb16754947cb4120c
soundrapanndiyan7 <soundrapanndiyan2468@gmail.com>
2025-03-12 15:21:20 +0530

    Hari Haran

---
+++ ex5.java
@@ -0,0 +1,7 @@
+public class ex5{
+    public static void main(String[]agrs){
+       String firstname="Hari";
+       String secondname="Haran";
+        System.out.print(firstname+secondname);
+    }
+}
