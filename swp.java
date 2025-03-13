fc13d061c529e34e3b221f12eb16ec3b39f3313a
soundrapanndiyan7 <soundrapanndiyan2468@gmail.com>
2025-03-13 14:03:45 +0530

    op

---
+++ swp.java
@@ -0,0 +1,10 @@
+public class swp{
+    public static void main(String[]args){
+         int a = 5;
+        int b = 10;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = " + a + ", b = " + b);
+    }
+}
