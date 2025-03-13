0eddae6a0b1d276363e4b35f12f6f519ba7cc361
soundrapanndiyan7 <soundrapanndiyan2468@gmail.com>
2025-03-13 15:16:15 +0530

    rrrp

---
+++ ss.java
@@ -0,0 +1,13 @@
+public  class ss{
+    public static void  main(String[]args){
+   int age=16;   
+    if(age==18){
+        System.out.println("he is mager");}
+    else if(age>=18){
+         System.out.println("he is mager");
+    }
+             else {
+                System.out.println("he is not mager");
+        }
+    }
+    }
