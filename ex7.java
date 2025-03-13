4590752907084b5a9a7cf54eb1e7a31d796ad29a
soundrapanndiyan7 <soundrapanndiyan2468@gmail.com>
2025-03-13 10:04:51 +0530

    name

---
+++ ex7.java
@@ -0,0 +1,13 @@
+public class ex7{
+    public static void main(String[]args)
+    {
+        String Name="sakthi";
+        System.out.println("Enter your name:"+Name);
+        String Sub="Maths";
+         System.out.println("Enter your subject:"+Sub);
+         int Mark=95;
+         System.out.println("Enter your mark:"+Mark);
+         String Grade="o";
+          System.out.println("your grade:"+Grade);
+            }
+    }
