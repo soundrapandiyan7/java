12cdf267bac2b7a937b40d3bb4f2a298ea97ba14
soundrapanndiyan7 <soundrapanndiyan2468@gmail.com>
2025-03-13 11:10:51 +0530

    add

---
+++ ass.java
@@ -0,0 +1,8 @@
+public class ass{
+    public static void main(String[]args){
+        int x=34;
+        int y=5;
+        System.out.println("Addition:"+(x+y));
+        System.out.println("Multi:"+x*y);
+    }
+}
