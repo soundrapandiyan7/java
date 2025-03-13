82697e07e887c9b388f123d642e53c4a94e4f755
soundrapanndiyan7 <soundrapanndiyan2468@gmail.com>
2025-03-13 12:17:51 +0530

    op

---
+++ op.java
@@ -0,0 +1,16 @@
+public  class op{
+    public static void  main(String[]args){
+        int a=55;
+        int b=8;
+    if(a<b && a>b){
+        System.out.println("for && it is true");}
+             else {
+                System.out.println("for && it is false");
+        }
+         if(a<b || a>b){
+            System.out.println("for || it is true");}
+             else {
+                System.out.println("for || it is false");
+    }
+    }
+}
