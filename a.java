b9c0a321ef7d983f56070de6be4c2c1c493fe83b
soundrapanndiyan7 <soundrapanndiyan2468@gmail.com>
2025-03-13 15:47:37 +0530

    dayy

---
+++ a.java
@@ -0,0 +1,16 @@
+public class a{
+    public static void main(String[]args){
+        int days=4;
+        switch(days){
+            case 1 : System.out.println("monday");
+            break;
+            case 2 : System.out.println("tuesday");
+            break;
+            case 3 : System.out.println("friday");
+            break;
+            default:
+                System.out.println("wednesday");
+        }
+    }
+}        
+        
