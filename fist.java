1541a00aa65fdf9728927965b5d0559f89a5195a
soundrapanndiyan7 <soundrapanndiyan2468@gmail.com>
2025-03-14 15:38:54 +0530

    ggg

---
+++ fist.java
@@ -0,0 +1,13 @@
+class student{
+    int id;
+    String name;
+}
+public class fist{
+    public static void main(String[]args){
+        student s1=new student();
+        s1.id=1;
+        System.out.println(s1.id);
+        s1.name="hari";
+        System.out.println(s1.name);
+    }
+} 
