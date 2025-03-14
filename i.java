fa8062f10bc869a8002e8b86f654ddd7b7103473
soundrapanndiyan7 <soundrapanndiyan2468@gmail.com>
2025-03-14 10:13:58 +0530

    strig

---
+++ i.java
@@ -0,0 +1,11 @@
+public  class i{
+    public static void  main(String[]args){
+      String hy="helo";
+        if(hy.contains("helo")){
+            System.out.println("it is String"); 
+        }
+        else{
+            System.out.println("it is not string");
+        }  
+    }
+}
