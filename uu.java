35060845a43ebe45eb5d5504935381323e0acf5f
soundrapanndiyan7 <soundrapanndiyan2468@gmail.com>
2025-03-14 11:48:37 +0530

    strig

---
+++ uu.java
@@ -0,0 +1,12 @@
+public  class uu
+{
+    public static void  main(String[]args){
+        int i;
+    for(i=0;i<=5;i++){
+        System.out.println(i);
+    if(i==3){
+        break;
+    }
+    }
+    }
+    }
