09e4234794d627a2fa404158d99b67f8a7ac949e
soundrapanndiyan7 <soundrapanndiyan2468@gmail.com>
2025-03-14 14:53:41 +0530

    ggg

---
+++ ar1.java
@@ -0,0 +1,7 @@
+public class ar1{
+    public static void main(String[]args){
+        String [] bikes={"kawasaki","ktm","bmw","re","yamaha"};
+        System.out.println(bikes[1]="duke");
+        System.out.println(bikes[1] );
+    }
+    }
