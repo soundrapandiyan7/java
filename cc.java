93bc42760ccce26fba15003434fe15a694b369f4
soundrapanndiyan7 <soundrapanndiyan2468@gmail.com>
2025-03-14 13:14:12 +0530

    hhh

--- cc.java
+++ cc.java
@@ -1,12 +1,10 @@
-public  class cc
+public  class cc{
+    static void mymethod()
+    {
+        System.out.println("bike");
+            }
+public static void main(String[]args)
 {
-    public static void  main(String[]args){
-        int i;
-    for(i=0;i<=5;i++){
-        System.out.println(i);
-    if(i==3){
-        continue;
-    }
-    }
-    }
-    }
+    mymethod();
+}
+}
