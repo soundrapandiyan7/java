7cd05e38f12a853cc0393cd5fd768742c802e374
soundrapanndiyan7 <soundrapanndiyan2468@gmail.com>
2025-03-14 11:50:33 +0530

    strig

---
+++ cc.java
@@ -0,0 +1,12 @@
+public  class cc
+{
+    public static void  main(String[]args){
+        int i;
+    for(i=0;i<=5;i++){
+        System.out.println(i);
+    if(i==3){
+        continue;
+    }
+    }
+    }
+    }
