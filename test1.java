02acc4a93572e97f277b062744f34f41aa3713e8
soundrapanndiyan7 <soundrapanndiyan2468@gmail.com>
2025-03-15 13:47:58 +0530

    jjj

---
+++ test1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class test1 {
+    public static void main(String[] args) {
+        Scanner mark = new Scanner(System.in);
+        System.out.print("Enter the mark:");
+      int gg=mark.nextInt();
+     if(gg>=90){
+            System.out.println("grade A");
+        }
+        else if(gg>=75){
+            System.out.println("grade B");
+        }
+        else if(gg>=50){
+            System.out.println("grade C");
+        }
+        else{
+            System.out.println("grade F");
+        }
+    }
+}
