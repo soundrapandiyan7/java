c6824d8a4394c922560ed9bc309bc8c86ea3124e
soundrapanndiyan7 <soundrapanndiyan2468@gmail.com>
2025-03-15 12:07:23 +0530

    jjj

---
+++ y.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class y{
+    public static void main(String[]agrs){
+Scanner elo=new Scanner(System.in);
+System.out.print("Enter the num:");
+int Num=elo.nextInt();
+System.out.println("the num:" +Num);
+    }}
