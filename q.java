9bcd5b115101f579ef47d6b4b86e431b7d355f34
soundrapanndiyan7 <soundrapanndiyan2468@gmail.com>
2025-03-15 11:06:03 +0530

    jjj

---
+++ q.java
@@ -0,0 +1,16 @@
+public class q{
+    public static void main(String[]args){
+        int coffeshop=4;
+        switch(coffeshop){
+            case 1 : System.out.println("MS");
+            break;
+            case 2 : System.out.println("CS");
+            break;
+            case 3 : System.out.println("FS");
+            break;
+            default:
+                System.out.println("NOT AVAILABLE");
+        }
+    }
+}        
+        
